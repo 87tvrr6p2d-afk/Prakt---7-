3646ce268af407d31b7f1f660d300bcacaaa9206
87tvrr6p2d-afk <87tvrr6p2d@privaterelay.appleid.com>
2026-03-04 11:23:07 +0500

    Create PdfReport.java

---
+++ PdfReport.java
@@ -0,0 +1,10 @@
+class PdfReport extends ReportGenerator {
+
+    void fetchData() {
+        System.out.println("Fetching data for PDF report");
+    }
+
+    void formatData() {
+        System.out.println("Formatting data as PDF");
+    }
+}
