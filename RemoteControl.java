55ab7177e966843298873b7372c80ab5ad391bc3
87tvrr6p2d-afk <87tvrr6p2d@privaterelay.appleid.com>
2026-03-04 11:20:56 +0500

    Create RemoteControl.java

---
+++ RemoteControl.java
@@ -0,0 +1,20 @@
+public class RemoteControl {
+
+    private ICommand command;
+    private ICommand lastCommand;
+
+    public void setCommand(ICommand command) {
+        this.command = command;
+    }
+
+    public void pressButton() {
+        command.execute();
+        lastCommand = command;
+    }
+
+    public void pressUndo() {
+        if (lastCommand != null) {
+            lastCommand.undo();
+        }
+    }
+}
