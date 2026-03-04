034d829752ffffeb133c6fc57c65aafcc4ccf463
87tvrr6p2d-afk <87tvrr6p2d@privaterelay.appleid.com>
2026-03-04 11:23:36 +0500

    Create ExcelReport.java

---
+++ ExcelReport.java
@@ -0,0 +1,10 @@
+class ExcelReport extends ReportGenerator {
+
+    void fetchData() {
+        System.out.println("Fetching data for Excel report");
+    }
+
+    void formatData() {
+        System.out.println("Formatting data as Excel");
+    }
+}
