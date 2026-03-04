d37419d2865d589b5f58346bdf4fa66787081c44
87tvrr6p2d-afk <87tvrr6p2d@privaterelay.appleid.com>
2026-03-04 11:24:00 +0500

    Create HtmlReport.java

---
+++ HtmlReport.java
@@ -0,0 +1,10 @@
+class HtmlReport extends ReportGenerator {
+
+    void fetchData() {
+        System.out.println("Fetching data for HTML report");
+    }
+
+    void formatData() {
+        System.out.println("Formatting data as HTML");
+    }
+}
